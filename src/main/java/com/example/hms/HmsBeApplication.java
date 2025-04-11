950ac359e544d878b2a79141fbd49dc95a42ccad
tiendung311 <109157322+tiendung311@users.noreply.github.com>
2025-04-11 08:26:42 +0700

    Initial commit

---
+++ src/main/java/com/example/hms/HmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hms/HmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
